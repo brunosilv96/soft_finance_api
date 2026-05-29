1a80f9770e1bbb04d45fd63d9417aa45815a1304
Bruno Silva <bruno.silv96@hotmail.com>
2026-05-29 17:28:53 -0300

    feat: class comprehention

--- src/roadmap/classes.py
+++ src/roadmap/classes.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+
+# Create automatically init, repr and eq (DTO and Entities)
+@dataclass
+class Customer:
+    id: str
+    name: str
+    email: str
+
+# Traditional class (general purpose)
 class User:
     # Constructor
     def __init__(self, name: str, age: int):
@@ -11,8 +21,21 @@ class User:
     def majority(self) -> bool:
         return self.age >= 18
     
+    # Factory Method - Return a instance of User
+    @classmethod
+    def empty(cls) -> User:
+        return cls('', 0)
+    
+    # Static Method - Without access to class (cls) or object (self) 
+    @staticmethod
+    def random() -> None:
+        print('Alguma coisa aleatória')
+    
 
 newUser: User = User(name='Brunão', age=29)
+emptyUser: User = User.empty()
+
+emptyUser.random()
 
 newUser.greeting()
 
